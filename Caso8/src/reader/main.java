2e2a15fd31d7573f138dc5e7908758145ed615f0
Dylan Stef Torres Walker <48809822+StefWalker@users.noreply.github.com>
2019-10-05 22:25:38 -0600

    Changes

--- Caso8/src/reader/main.java
+++ Caso8/src/reader/main.java
@@ -7,32 +7,41 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-public class main {
-	public static void check() {
-		try {
-			BufferedImage image = ImageIO.read(new File("/reader/Images/Cubo.jpg"));
-			
-			int[][] pixels = new int[1024][1024];
-			for( int i = 0; i < 1024; i++ ) {
-			    for( int j = 0; j < 1024; j++ ) {
-			        pixels[i][j] = image.getRGB( i, j );
-			        Color mycolor = new Color(image.getRGB(i, j));
-			        //System.out.print(mycolor);
-			        //System.out.print(mycolor.getBlue());
-			        //System.out.print(mycolor.getGreen());
-			        //System.out.print(mycolor.getRed());
-			        
-			    }
-			    System.out.println("");
-			}
-			
-		}
-		catch(IOException e) {
-			
-		}
-	}
-	public static void main(String[] args ) {
-		check();
-	}
-	
+public class main
+{
+
+  public main()
+  {
+    try
+    {
+      // the line that reads the image file
+      BufferedImage image = ImageIO.read(new File("C://Users//Dylan//Desktop//Prog//image1.jpeg"));
+      System.out.println("Founded");
+      //System.out.println(image.getData());
+      //System.out.println(image.getColorModel());
+      //System.out.println(image.getGraphics());
+      //System.out.println(image.getRGB(500, 500));
+      
+      int[][] pixels = new int[1024][1024];
+
+      for( int i = 0; i < 1024; i++ ) {
+          for( int j = 0; j < 1024; j++ ) {
+              pixels[i][j] = image.getRGB( i, j );
+      		  System.out.print(pixels[i][j]);
+          }
+          System.out.println("");
+      }	  
+    } 
+    catch (IOException e)
+    {
+      // log the exception
+      // re-throw if desired
+    }
+  }
+
+  public static void main(String[] args)
+  {
+    new main();
+  }
+
 }
