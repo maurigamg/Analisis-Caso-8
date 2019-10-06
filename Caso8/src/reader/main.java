c1e142025748172847ab94c29896c064382c653a
Dylan Stef Torres Walker <48809822+StefWalker@users.noreply.github.com>
2019-10-06 03:33:15 -0600

    image cutter

--- Caso8/src/reader/main.java
+++ Caso8/src/reader/main.java
@@ -2,20 +2,23 @@ package reader;
 
 
 import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class main
 {
 
-  public main()
+  public static void main()
   {
     try
     {
       // the line that reads the image file
-      BufferedImage image = ImageIO.read(new File("C://Users//Dylan//Desktop//Prog//image1.jpeg"));
+      BufferedImage image = ImageIO.read(new File("Direccion de la imagen"));
       System.out.println("Founded");
       //System.out.println(image.getData());
       //System.out.println(image.getColorModel());
@@ -38,10 +41,44 @@ public class main
       // re-throw if desired
     }
   }
+  
+  public static void imageSplit() throws IOException{
+	  File file = new File("Direccion de la imagen"); // I have bear.jpg in my working directory
+      FileInputStream fis = new FileInputStream(file);
+      BufferedImage image = ImageIO.read(fis); //reading the image file
 
-  public static void main(String[] args)
+      int rows = 4; //You should decide the values for rows and cols variables
+      int cols = 4;
+      int chunks = rows * cols;
+
+      int chunkWidth = image.getWidth() / cols; // determines the chunk width and height
+      int chunkHeight = image.getHeight() / rows;
+      int count = 0;
+      BufferedImage imgs[] = new BufferedImage[chunks]; //Image array to hold image chunks
+      for (int x = 0; x < rows; x++) {
+          for (int y = 0; y < cols; y++) {
+              //Initialize the image array with image chunks
+              imgs[count] = new BufferedImage(chunkWidth, chunkHeight, image.getType());
+
+              // draws the image chunk
+              Graphics2D gr = imgs[count++].createGraphics();
+              gr.drawImage(image, 0, 0, chunkWidth, chunkHeight, chunkWidth * y, chunkHeight * x, chunkWidth * y + chunkWidth, chunkHeight * x + chunkHeight, null);
+              gr.dispose();
+          }
+      }
+      System.out.println("Splitting done");
+
+      //writing mini images into image files
+      for (int i = 0; i < imgs.length; i++) {
+          ImageIO.write(imgs[i], "jpg", new File("img" + i + ".jpg"));
+      }
+      System.out.println("Mini images created");
+  }
+
+  public static void main(String[] args) throws IOException
   {
-    new main();
+    main();
+    imageSplit();
   }
 
 }
