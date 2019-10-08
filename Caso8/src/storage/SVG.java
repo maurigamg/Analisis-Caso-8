796ed3e734bf3f4b59664923bf4c0a01aa5045e8
Dylan Stef Torres Walker <steftorres927@gmail.com>
2019-10-07 18:30:43 -0600

    SVG

--- Caso8/src/storage/SVG.java
+++ Caso8/src/storage/SVG.java
@@ -17,7 +17,7 @@ public class SVG {
    * This method creates a svg file
    * @throws IOException
    */
-  public static void SVGgenerator() throws IOException {
+  public static void SVGgenerator(int color, int value0, int value1) throws IOException {
     String svg = "";
     svg += "<?xml version=\"1.0\" standalone=\"no\"?>\n";
     svg += "<svg width=\"1024\" height=\"1024\" version=\"1.1\"\n";
@@ -25,12 +25,12 @@ public class SVG {
     svg += "  <desc>Four separate rectangles\n";
     svg += "  </desc>\n";
 
-    svg += "  <path vector-effect=\"none\" fill=\"#808000\" d=\"M50,80 L80,50 L50,50 L50,80\"/>\n";
-    int k = 1;
+    int k = 1; // Change to array size
     for(int i = 0; i != k; i++) 
     {
-      svg += "<path vector-effect=\"none\" fill=\"#808000\" d=\"M50,80 L80,50 L50,50 L50,80\"/>";
-      //svg += "  <path vector-effect=\"none\" fill=\"" + color +"\" d=\"M" + 50,80 + " L" + 80,50 + " L" + 50,50 " L" + 50,80 + "/>\n";
+      svg += "<path vector-effect=\"none\" fill=\"#808000\" d=\"M50,80 L80,50 L50,50 L50,80 L80,80\"/>";
+      svg += "  <path vector-effect=\"none\" fill=\"" + color +"\" d=\"M" + value0 + "," + value1 + " L" + value1 + "," + value0 + " L" + value0 + "," + value0 + 
+    		  " L" + value0 + "," + value1 + " L" + value1 + "," + value1 + "/>\n";
     }
     svg += "  <!-- Show outline of viewport using 'rect' element -->\n";
     svg += "  <rect x=\".10cm\" y=\".10cm\" width=\"1024\" height=\"1024\"\n";
